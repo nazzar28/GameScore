8897ccf345f6ff010dab247da2c49aa35cc9adc5
nazzar28 <talantbekovnazar28@gmail.com>
2024-07-10 15:48:39 +0600

    done

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gamescore;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int score1 = 0;
+    private int score2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView counter1 = findViewById(R.id.textViewCounter1);
+        TextView counter2 = findViewById(R.id.textViewCounter2);
+
+        counter1.setText(String.valueOf(score1));
+        counter2.setText(String.valueOf(score2));
+
+        counter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter1.setText(String.valueOf(++score1));
+            }
+        });
+
+        counter2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter2.setText(String.valueOf(++score2));
+            }
         });
     }
 }
